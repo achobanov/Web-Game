d335250eacf52f1a9be22d54ba431c8429b90d28
achobanov <alexchobanov@gmail.com>
2020-02-02 16:12:41 +0200

    Babel module in webpack https://github.com/achobanov/Web-Game/issues/3

--- Source/Client/game.js
+++ Source/Client/game.js
@@ -1,5 +1,5 @@
 class Game {
-    run() { console.log("Game started"); }
+    run = () => console.log("Game started");
 }
 
 export { Game }
--- Source/Client/webpack.config.js
+++ Source/Client/webpack.config.js
@@ -2,8 +2,24 @@ const path = require('path');
 
 module.exports = {
   entry: './app.js',
+  mode: 'development',
   output: {
     filename: 'vendor.js',
     path: path.resolve(__dirname, '../Server/webroot/'),
   },
-};
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      } 
+    ]
+  },
+}; 
