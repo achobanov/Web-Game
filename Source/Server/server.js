d41d12510a322eded5b84b1bf4cf2a1ac347b668
achobanov <alexchobanov@gmail.com>
2020-02-02 17:16:03 +0200

    Removed module-loader and server index.html https://github.com/achobanov/Web-Game/issues/3

---
+++ Source/Server/server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+fs.readFile('./webroot/index.html', function(err, html) {
+  if (err) { throw err }
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(html);
+    res.end();
+  });
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+});
