310a25d764e54faa116f1aa09e9f5c1191cad055
achobanov <alexchobanov@gmail.com>
2020-02-05 22:51:18 +0200

    Added frame properties and changeFrame method https://github.com/achobanov/Web-Game/issues/4

--- Source/Client/source/objects/base/entity.ts
+++ Source/Client/source/objects/base/entity.ts
@@ -6,14 +6,49 @@ export interface IEntity {
 
 export default class Entity extends GameObject implements IEntity {
     speed: number;
+    frames: Array<Array<number>>;
+    frameIndex: number;
+    frameRate: number;
+    hasMoved : boolean;
+    hasChangedFrame: boolean;
 
-    constructor(x: number, y: number, width: number, height: number, speed: number, image: string) {
+    constructor(
+        x: number, 
+        y: number, 
+        width: number,
+        height: number,
+        speed: number,
+        image: string,
+        frames: Array<Array<number>>,
+        frameRate: number,
+        frameIndex?: number,
+    ) {
         super(x, y, width, height, image);
 
         this.speed = speed;
+        this.frames = frames;
+        this.frameIndex = frameIndex ?? 0;
+        this.frameRate = frameRate;
+        this.hasMoved = false;
+        this.hasChangedFrame = false;
     }
 
-    update = (dt : number) => {
-        return false;
+    update = (dt : number) : boolean => {
+        const hasUpdated = this.hasMoved || this.hasChangedFrame;
+
+        this.hasMoved = false;
+        this.hasChangedFrame = false;
+
+        return hasUpdated;
+    }
+        
+
+    changeFrame = (index: number) : void => {
+        if (!this.frames[index]) {
+            throw new RangeError('Index outside of frames array.');
+        }
+
+        this.frameIndex = index;
+        this.hasChangedFrame = true;
     }
 }
