5b83c062d3d68ba3c937ff47bacd94db3cc3e59f
achobanov <alexchobanov@gmail.com>
2020-02-05 23:04:22 +0200

    Added imageKey to ISprite and ResourcesService dependency https://github.com/achobanov/Web-Game/issues/4

--- Source/Client/source/services/render-service.ts
+++ Source/Client/source/services/render-service.ts
@@ -1,24 +1,29 @@
+import ResourcesService from "./resources-service";
+
 export interface ISprite {
     x: number,
     y: number,
     width: number,
     height: number,
+    imageKey: number,
     frameIndex: number,
     frames: Array<Array<number>>
 }
 
 export default class RenderService {
     _context : CanvasRenderingContext2D;
+    _resourcesService : ResourcesService;
 
-    constructor(canvas: HTMLCanvasElement) {
+    constructor(canvas: HTMLCanvasElement, resourcesService: ResourcesService) {
         const context = canvas.getContext('2d');
         
         if (!context) throw new Error('Context not found!');
 
         this._context = context;
+        this._resourcesService = resourcesService;
     }
 
     render = (sprite: ISprite) : void => {
-        
+
     }
 }
