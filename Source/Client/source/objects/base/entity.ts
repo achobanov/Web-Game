6f49b3d44369c4bb1e4b6e78039d9f603c45fa69
achobanov <alexchobanov@gmail.com>
2020-02-05 23:59:52 +0200

    Entity now implements IEntity, ISprite https://github.com/achobanov/Web-Game/issues/4

--- Source/Client/source/objects/base/entity.ts
+++ Source/Client/source/objects/base/entity.ts
@@ -1,18 +1,18 @@
 import GameObject from "./game-object";
 import { ISprite, IRenderable } from "../../services/render-service";
 
-export interface IUpdateable {
+export interface IEntity extends ISprite {
     update: (dt : number) => void;
 }
 
-export default class Entity extends GameObject implements ISprite {
+export default class Entity extends GameObject implements IEntity {
     uid: string;
     speed: number;
     frames: Array<IRenderable>;
     frameIndex: number;
     frameRate: number;
     frame: IRenderable;
-
+    
     constructor(
         uid: string,
         x: number, 
@@ -26,7 +26,7 @@ export default class Entity extends GameObject implements ISprite {
         frameIndex?: number,
     ) {
         super(x, y, width, height, imageKey);
-
+        
         this.uid = uid;
         this.speed = speed;
         this.frames = frames;
@@ -34,6 +34,10 @@ export default class Entity extends GameObject implements ISprite {
         this.frameRate = frameRate;
         
         this.frame = this.frames[this.frameIndex];
+    }
+        
+    update = (dt: number) : void => {
+        throw new Error('"IEntity.update" must be overriden!');
     }
 
     changeFrame = (index: number) : void => {
