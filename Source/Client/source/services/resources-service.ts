74fc383bec8c2b584f4bd7eb768cbaf5cedfcd81
achobanov <alexchobanov@gmail.com>
2020-02-05 22:33:55 +0200

    Method to parse and retrieve frames from sprite map https://github.com/achobanov/Web-Game/issues/

--- Source/Client/source/services/resources-service.ts
+++ Source/Client/source/services/resources-service.ts
@@ -21,4 +21,35 @@ export default class ResourcesService {
             resolve();            
         });
     }
+
+    parseFrames = (key: string, framesCount: number) : Array<Array<number>> => {
+        const image = this.images[key];
+
+        let frameWidth;
+        let frameHeight;
+
+        var isHorizontalMap = image.width > image.height;
+
+        if (isHorizontalMap) {
+            frameHeight = image.height;
+            frameWidth = image.width / framesCount;
+        } else {
+            frameHeight = image.height / framesCount;
+            frameWidth = image.width;
+        }
+
+        const frames = [];
+        let frameX = 0;
+        let frameY = 0;
+        for (let i = 0; i < framesCount; i++) {
+            frames.push([ frameX, frameY, frameWidth, frameHeight ]);
+            
+            if (isHorizontalMap)
+                frameX += frameWidth;
+            else
+                frameY == frameHeight;
+        }
+
+        return frames;
+    }
 }
