b9df24e77c3b35071c12cd761359fd0a721c13ee
achobanov <alexchobanov@gmail.com>
2020-02-08 22:42:58 +0200

    SetupService in game.ts https://github.com/achobanov/Web-Game/issues/4

--- Source/Client/source/game.ts
+++ Source/Client/source/game.ts
@@ -4,29 +4,30 @@ import InputService from "./services/input-service";
 import EventsService from "./services/events-service";
 import RenderService from "./services/render-service";
 import { IEntity } from "./objects/base/entity";
+import SetupService from "./services/setup-service";
 
 export default class Game {
-    _images: ImagesService;
     _input: InputService;
     _events: EventsService;
     _render: RenderService;
+    _setup: SetupService
 
     _previousFrameTime: number = 0;
     _entities: IEntity[];
 
     constructor(container: Node, settings: ILaunchSettings) {
-        this._images = new ImagesService(settings.imagePaths);
+        const images = new ImagesService(settings.imagePaths);
+        
         this._events = new EventsService();
         this._input = new InputService(container, this._events);
-        this._render = new RenderService(container as HTMLCanvasElement, this._images);
+        this._render = new RenderService(container as HTMLCanvasElement, images);
+        this._setup = new SetupService(images);
 
         this._entities = [];
     }
 
     start = async () : Promise<void> => {
-        await this._images.haveLoaded;
-
-        // Add initial entities;
+        this._entities = await this._setup.proofOfConcept();
 
         this._previousFrameTime = Date.now();
 
