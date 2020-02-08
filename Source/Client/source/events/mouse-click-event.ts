67085afa7a16ce166c049f8b64a2b9f9777ca036
achobanov <alexchobanov@gmail.com>
2020-02-08 23:50:19 +0200

    Added static keys on event to ease subscription https://github.com/achobanov/Web-Game/issues/4

--- Source/Client/source/events/mouse-click-event.ts
+++ Source/Client/source/events/mouse-click-event.ts
@@ -2,7 +2,9 @@ import { IEvent } from "../services/events-service";
 import { MouseButton } from "../enums/mouse-button";
 
 export default class MouseClickEvent implements IEvent {
-    key: string = 'mouse-click';
+    static Key = 'mouse-click';
+
+    key = MouseClickEvent.Key;
     button: MouseButton;
 
     constructor(button: MouseButton) {
--- Source/Client/source/events/mouse-move-event.ts
+++ Source/Client/source/events/mouse-move-event.ts
@@ -2,7 +2,9 @@ import { IEvent } from "../services/events-service";
 import { ICursorData } from "../services/input-service";
 
 export default class MouseMoveEvent implements IEvent {
-    key: string = 'mouse-move';
+    static Key = 'mouse-move';
+
+    key = MouseMoveEvent.Key;
     cursor : ICursorData;
 
     constructor(cursor: ICursorData) {
