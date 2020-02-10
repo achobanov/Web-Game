649e5ef46caa72bd3444ebda47eb0ee5109d2c0f
achobanov <alexchobanov@gmail.com>
2020-02-10 20:27:17 +0200

    Hover indicator https://github.com/achobanov/Web-Game/issues/1

---
+++ Source/Client/source/objects/entities/hover-indicator.ts
@@ -0,0 +1,30 @@
+import Circle from "../shapes/circle";
+import EventsService from "../../services/events-service";
+import MouseMoveEvent from "../../events/mouse-move-event";
+import RemoveEntityEvent from "../../events/remove-entity-event";
+import MouseClickEvent from "../../events/mouse-click-event";
+import { MouseButton } from "../../enums/mouse-button";
+
+export default class HoverIndicator extends Circle {
+    _events: EventsService;
+
+    constructor(events: EventsService, id: string, x: number, y: number, radius: number, fill: string) {
+        super(id, x, y, radius, fill);
+
+        this._events = events;
+
+        this._events.subscribe(MouseMoveEvent.Key, this._handleMove);
+        this._events.subscribe(MouseClickEvent.Key, this._handleClick);
+    }
+
+    _handleMove = ({ cursor }: MouseMoveEvent) => {
+        const { x, y } = cursor;
+        this.x = x - this.radius * 0.75;
+        this.y = y - this.radius * 0.75;
+    }
+
+    _handleClick = ({ button }: MouseClickEvent) => {
+        if (button === MouseButton.Left)
+            this._events.publish(new RemoveEntityEvent(this.id));
+    }
+}
