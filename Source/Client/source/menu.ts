759baebfb35b5deeb85f2998da23e8005e07bb73
achobanov <alexchobanov@gmail.com>
2020-02-10 20:29:09 +0200

    Added indicator on button hover https://github.com/achobanov/Web-Game/issues/1

--- Source/Client/source/menu.ts
+++ Source/Client/source/menu.ts
@@ -8,6 +8,9 @@ import RemoveEntityEvent from "./events/remove-entity-event";
 import MouseMoveEvent from "./events/mouse-move-event";
 import MouseClickEvent from "./events/mouse-click-event";
 import { MouseButton } from "./enums/mouse-button";
+import Circle from "./objects/shapes/circle";
+import { ICoordinates } from "./services/canvas-service";
+import HoverIndicator from "./objects/entities/hover-indicator";
 
 export default class Menu implements IGameObject {
     id: string;
@@ -26,6 +29,7 @@ export default class Menu implements IGameObject {
     _background: Rectangle;
     _button: Rectangle;
     _speed: number;
+    _hoverIndicatorId: string;
     _startGame: () => void;
     
     constructor(events: EventsService, startGame: () => void) {
@@ -37,7 +41,8 @@ export default class Menu implements IGameObject {
         this._closingTime = 0;
         this._startGame = startGame;
         this._isButtonHovered = false;
-
+        this._hoverIndicatorId = utils.uId();
+        
         this.id = utils.uId();
         this.x = 0;
         this.y = 0;
@@ -81,12 +86,25 @@ export default class Menu implements IGameObject {
         }
     }
 
-    _onMouseMove = (event: MouseMoveEvent) =>
+    _createIndicator({ x, y }: ICoordinates) {
+        const indicator = new HoverIndicator(this._events, this._hoverIndicatorId, x, y, 10, 'yellow');
+        this._events.publish(new AddEntityEvent(indicator));
+    }
+
+    _onMouseMove = (event: MouseMoveEvent) => {
+        const wasButtonHovered = this._isButtonHovered;
+
         this._isButtonHovered = 
             this._button.x < event.cursor.x
-         && this._button.x + this._button.width > event.cursor.x
-         && this._button.y < event.cursor.y
-         && this._button.y + this._button.width > event.cursor.y;
+            && this._button.x + this._button.width > event.cursor.x
+            && this._button.y < event.cursor.y
+            && this._button.y + this._button.height > event.cursor.y;
+
+        if (this._isButtonHovered && !wasButtonHovered)
+            this._createIndicator(event.cursor);
+        else if (!this._isButtonHovered && wasButtonHovered)
+            this._events.publish(new RemoveEntityEvent(this._hoverIndicatorId));        
+    }
 
     _onMouseClick = (event: MouseClickEvent) =>
         this._isButtonHovered && event.button === MouseButton.Left && this._startClosing();
